edee977dd58cbbaf2a1e0e8c57c28e974c128db1
Sandeep Reddy <sandeep.gandluri@gmail.com>
2024-11-17 09:48:44 -0600

    Dev Environment Setup

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text style={{fontSize: 50}}>My First React Native Application</Text>;
+};
+
+export default App;
